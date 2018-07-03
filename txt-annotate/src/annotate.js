777734b59131d2a3dd8e07d91c0409f4b47338f1
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-07-03 12:26:17 +0300

    create entities

--- txt-annotate/src/annotate.js
+++ txt-annotate/src/annotate.js
@@ -13,7 +13,7 @@ function highlight(o) {
   for (const key of Object.keys(o)){
    if (key !== 'content' && key !== 'id'){
      for (const hl of o[key]){
-       t = t.replace(new RegExp(escapeRegExp(hl), 'g'),'<span class="field">'+hl+'<span class="tooltip"> '+key+' </span></span>')
+       t = t.replace(new RegExp(escapeRegExp(hl), 'g'),'<span class="field" draggable="true" ondragstart="dragstart_handler(event)">'+hl+'<span class="tooltip"> '+key+' </span></span>')
      }
    }
   }
@@ -35,25 +35,56 @@ function findKeyByValue(o,value){
 
 
 export function annotate(e,o) {
-
+    const userobjects = []
     const parent = document.createElement("div")
     const text = document.createElement("div")
     // TODO something else than innerHTML
     text.innerHTML = highlight(o)
 
-    const button = document.createElement('button');
-    button.innerHTML = 'save';
-    button.onclick = function(){
+    const save = document.createElement('button')
+    save.innerHTML = 'save';
+    save.onclick = function(){
       delete(o.content)
-      parent.innerHTML = '<pre>'+JSON.stringify(o,null,2)+'</pre>'
+      parent.innerHTML = '<pre>'+JSON.stringify(o,null,2)+JSON.stringify(userobjects,null,2)+'</pre>'
       return false
     }
+    const drops = document.createElement('div')
+    const adddrops = document.createElement('button')
+    adddrops.innerHTML = 'add new object';
+    adddrops.onclick = function(){
+      const id = userobjects.length
+      userobjects.push([])
+      const holder = document.createElement('span')
+      holder.className = 'holder'
+      holder.addEventListener('dragenter',() => { holder.className += " hovered" }, false)
+      holder.addEventListener('dragover',(event) => { event.preventDefault(); }, false)
+      holder.addEventListener('dragleave',() => { holder.className = "holder" }, false)
+      holder.addEventListener('drop',(event) => {
+        event.preventDefault();
+        const value = dragged.childNodes[0].data
+        const key = dragged.childNodes[1].childNodes[0].data.trim()
+        console.log(key,value)
+        const tmp = {}
+        tmp[key] = value
+        userobjects[id].push(tmp)
+        const li = document.createElement('li')
+        li.innerText = id+' : '+key+' : '+value
+        holder.appendChild(li)
+      }, false)
+      drops.appendChild(holder)
+      return false
+    }
+
+
+
     parent.appendChild(text)
-    parent.appendChild(button)
+    parent.appendChild(drops)
+    parent.appendChild(adddrops)
+    parent.appendChild(save)
     e.appendChild(parent)
 
     function getSelectionString(e) {
-
+      console.log('getSelectionString',e)
       const selection = document.getSelection()
       const value = selection.toString().trim()
       if (value !== '' ) {
