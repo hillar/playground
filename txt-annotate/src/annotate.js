cfd8dcc8f25fc20dd6be7ec9a72d640e22cf0125
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-07-04 10:35:11 +0300

    content field as param

--- txt-annotate/src/annotate.js
+++ txt-annotate/src/annotate.js
@@ -7,12 +7,13 @@ function linebreaks(text){
   return text.replace(new RegExp('\n\n','g'),'<p>').replace(new RegExp('\n','g'),'<br>')
 }
 
-function highlight(o) {
-  if (!o.content) return ''
-  let t = o.content.join('\n\n')
+function highlight(o,contenFieldName) {
+  if (!o[contenFieldName]) return ''
+  let t = o[contenFieldName].join('\n\n')
   for (const key of Object.keys(o)){
-   if (key !== 'content' && key !== 'id'){
+   if (key !== 'content' && key !== 'id' && o[key] && Array.isArray(o[key])){
      for (const hl of o[key]){
+       console.log(key,hl)
        t = t.replace(new RegExp(escapeRegExp(hl), 'g'),'<span class="field" draggable="true" ondragstart="dragstart_handler(event)">'+hl+'<span class="tooltip"> '+key+' </span></span>')
      }
    }
@@ -20,9 +21,9 @@ function highlight(o) {
   return linebreaks(t)
 }
 
-function findKeyByValue(o,value){
+function findKeyByValue(o,value,contenFieldName){
   for (const key of Object.keys(o)){
-   if (key !== 'content' && key !== 'id'){
+   if (key !== contenFieldName && key !== 'id' && o[key] && Array.isArray(o[key])){
      for (const hl of o[key]){
        if (hl === value) {
          return key
@@ -34,18 +35,18 @@ function findKeyByValue(o,value){
 }
 
 
-export function annotate(e,o) {
+export function annotate(e,o,contenFieldName) {
     const userobjects = []
-    const parent = document.createElement("div")
+    const master = document.createElement("div")
     const text = document.createElement("div")
     // TODO something else than innerHTML
-    text.innerHTML = highlight(o)
+    text.innerHTML = highlight(o,contenFieldName)
 
     const save = document.createElement('button')
     save.innerHTML = 'save';
     save.onclick = function(){
-      delete(o.content)
-      parent.innerHTML = '<pre>'+JSON.stringify(o,null,2)+JSON.stringify(userobjects,null,2)+'</pre>'
+      delete(o[contenFieldName])
+      master.innerHTML = '<pre>'+JSON.stringify(o,null,2)+JSON.stringify(userobjects,null,2)+'</pre>'
       return false
     }
     const drops = document.createElement('div')
@@ -75,13 +76,11 @@ export function annotate(e,o) {
       return false
     }
 
-
-
-    parent.appendChild(text)
-    parent.appendChild(drops)
-    parent.appendChild(adddrops)
-    parent.appendChild(save)
-    e.appendChild(parent)
+    master.appendChild(text)
+    master.appendChild(drops)
+    master.appendChild(adddrops)
+    master.appendChild(save)
+    e.appendChild(master)
 
     function getSelectionString(e) {
       console.log('getSelectionString',e)
@@ -89,27 +88,27 @@ export function annotate(e,o) {
       const value = selection.toString().trim()
       if (value !== '' ) {
           if (e.altKey) {
-            const todel = findKeyByValue(o,value)
+            const todel = findKeyByValue(o,value,contenFieldName)
             if (todel) {
               // ask user
               const del = confirm(todel+':'+value+'<- remove that ..')
               if (del){
                 if (o[todel])  {
                   delete(o[todel])
-                  text.innerHTML = highlight(o)
+                  text.innerHTML = highlight(o,contenFieldName)
                 }
               }
             }
           } else {
             // ask user
-            const key = prompt(value +" <- What is that?",findKeyByValue(o,value))
+            const key = prompt(value +" <- What is that?",findKeyByValue(o,value,contenFieldName))
             if (key && key.trim() !== '') {
               let overwrite = true
               // ask user
               if ( o[key] )  overwrite = confirm(o[key]+' :: overwrite ?')
               if (overwrite) o[key] = [value]
               console.log('added',key,value)
-              text.innerHTML = highlight(o)
+              text.innerHTML = highlight(o,contenFieldName)
             }
           }
         }
--- txt-annotate/src/main.js
+++ txt-annotate/src/main.js
@@ -14,9 +14,22 @@ const mocks = [
   }
 ]
 
+
+
 const alist = document.getElementById('annotationslist')
+/*
 for (const doc of mocks) {
   const li = document.createElement("li")
   annotate(li,doc)
   alist.appendChild(li)
 }
+*/
+fetch('http://172.16.33.129:8983/solr/solrdefalutcore/select?q=id:114b1bf8ab29a4ea306bd72b82e10584')
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(solr) {
+    console.log(solr);
+    const doc = solr.response.docs[0]
+    annotate(alist,doc,'logline')
+  });
