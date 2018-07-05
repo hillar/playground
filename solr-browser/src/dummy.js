40156bfe204a4b5aabf6d90747d90fab53699199
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-07-05 14:58:58 +0300

    some dummy table

--- solr-browser/src/dummy.js
+++ solr-browser/src/dummy.js
@@ -41,6 +41,49 @@ export class SolrQuery extends HTMLElement {
           console.log(solr);
           if (solr) {
           self.innerHTML = `
+          <div justify-content:="" center="">
+            <div class="b-table">
+              <div class="table-wrapper">
+                <table class="table has-mobile-cards">
+                  <thead>
+                    <tr>
+                      <th width="40px"></th>
+                      <th class="">
+                        <div class="th-wrap">Score <span class="icon is-small" style="display: none;"><i class="mdi mdi-arrow-up"></i></span></div>
+                      </th>
+                      <th class="">
+                        <div class="th-wrap">Highlights <span class="icon is-small" style="display: none;"><i class="mdi mdi-arrow-up"></i></span></div>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr class="">
+                      <td class="chevron-cell"><a role="button"><span class="icon"><i class="mdi mdi-chevron-right mdi-24px"></i></span></a></td>
+                      <td data-label="Score" class=""><span>
+                           16.980799
+                       </span></td>
+                      <td data-label="Highlights" class=""><span><div> .. </div></span></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- pagination -->
+              <div class="level">
+                <div class="level-left"> &nbsp;<b>Total found</b>: ${solr.response.numFound} </div>
+                <div class="level-right">
+                  <div class="level-item">
+                    <div class="pagination"><a role="button" href="#" disabled="disabled" class="pagination-previous"><span class="icon"><i class="mdi mdi-chevron-left mdi-24px"></i></span></a> <a role="button" href="#" disabled="disabled" class="pagination-next"><span class="icon"><i class="mdi mdi-chevron-right mdi-24px"></i></span></a>
+                      <ul class="pagination-list">
+                        <li><a role="button" href="#" class="pagination-link is-current">
+                          ${solr.response.start}
+                      </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
               <pre> ${JSON.stringify(solr,null,2)} </pre>
           `
           } else {
@@ -80,3 +123,8 @@ export class SolrQuery extends HTMLElement {
   }
 
  }
+ /*
+
+
+
+ */
