03e6e6621a3a1526558079e8cf0a623578ecb27b
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-08-03 13:51:47 +0300

    helper to create missing tests

---
+++ reststatic/src/server/test/before.js
@@ -0,0 +1,23 @@
+const glob = require('glob')
+const fs = require('fs')
+const path = require('path')
+const files = glob.sync('src/server/src/**/*.js')
+
+
+for (const src of files) {
+  const test = src.replace('/src/','/test/').replace('.js','.test.js')
+  if (!fs.existsSync(test)){
+    console.error('no test for', src)
+    fs.writeFileSync(test,`
+    process.alias = 'test ${src}'
+    test = require('tape')
+    test('${src}', function (t) {
+      t.fail('no tests for ${src}')
+      t.end()
+    })
+
+    `)
+  }
+
+
+}
---
+++ reststatic/src/server/test/classes/logmethods.test.js
@@ -0,0 +1,7 @@
+
+    process.alias = 'test src/server/src/classes/logmethods.js'
+    test = require('tape')
+    test('src/server/src/classes/logmethods.js', function (t) {
+      //t.fail('no tests for src/server/src/classes/logmethods.js')
+      t.end()
+    })
